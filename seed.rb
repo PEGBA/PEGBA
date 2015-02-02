a7c820eace5174ec5660a5fd7d9e1833cad66f4c
Alvin Liang <liangalvin@yahoo.com>
2015-02-02 11:45:20 -0500

    database file created through sqlite3, url's into the shirts class

--- seed.rb
+++ seed.rb
@@ -7,55 +7,55 @@ require 'pry'
 Shirt.create({
 	color: "red",
 	quantity: 10,
-	img_url: "",
+	img_url: "http://www.stevecurtin.com/blog/wp-content/uploads/2011/09/red-t-shirt.jpg",
 	price: 10
 	})
 
 Shirt.create({
 	color: "blue",
 	quantity: 10,
-	img_url: "",
+	img_url: "http://superhosting.pro/files/images/ecommerce/blue_shirt.jpg",
 	price: 10
 	})
 
 Shirt.create({
 	color: "pink",
 	quantity: 10,
-	img_url: "",
+	img_url: "http://jwmft.ednet.ns.ca/WP/wp-content/uploads/2014/05/pinkshirt.png",
 	price: 10
 	})
 
 Shirt.create({
 	color: "green",
 	quantity: 10,
-	img_url: "",
+	img_url: "http://i.ebayimg.com/00/s/MTE0OFgxMjAw/z/S1wAAMXQskFRHyxq/$T2eC16FHJF8E9nnC6MK(BRHy)pw-hg~~60_35.JPG",
 	price: 10
 	})
 
 Shirt.create({
 	color: "black",
 	quantity: 10,
-	img_url: "",
+	img_url: "http://cdn.splitsider.com/wp-content/uploads/2012/05/tumblr_m4m270iUfv1r6dybk-e1338300619778.jpeg",
 	price: 10
 	})
 
 Shirt.create({
 	color: "white",
 	quantity: 10,
-	img_url: "",
+	img_url: "https://weslinde.files.wordpress.com/2011/11/white-shirt-4.jpg",
 	price: 10
 	})
 
 Shirt.create({
 	color: "yellow",
 	quantity: 10,
-	img_url: "",
+	img_url: "http://www.clker.com/cliparts/S/j/5/K/N/Z/yellow-t-shirt-icon-md.png",
 	price: 10
 	})
 
 Shirt.create({
 	color: "grey",
 	quantity: 10,
-	img_url: "",
+	img_url: "http://www.superheroden.com/images/Batman_Circle_Logo_Light_Gray_Shirt2.jpg",
 	price: 10
 	})
