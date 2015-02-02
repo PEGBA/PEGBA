46aec7db5c73f9e1370561c9f68f5e95dee08c80
Cfo613 <cforan13@gmail.com>
2015-02-02 18:21:31 -0500

    updated server file

--- lib/buyers.rb
+++ lib/buyers.rb
@@ -3,7 +3,4 @@ require 'active_record'
 class Buyer < ActiveRecord::Base
   has_many :purchases
   has_many :shirts
-  # def shirt
-  #   Shirt.find_by({id: shirtID}) #find_by returns 1
-  # end
 end
--- server.rb
+++ server.rb
@@ -19,22 +19,37 @@ get("/cart/:id") do
 end
 
 post('/addToCart/:id') do
-  shirt = Shirt.find_by({id: params[:id]})
-	buyer_hash = {
-    name: params[:name],
-    email: params[:email],
-    quantity: params[:quantity],
-    color: shirt.color,
-    shirt_id: params[:id]
-  }
-
-  buyer = Buyer.create(buyer_hash)
+  buyer = Buyer.find_by({email: params[:email]})
+  if buyer == nil
+    shirt = Shirt.find_by({id: params[:id]})
+    buyer_hash = {
+      name: params[:name],
+      email: params[:email],
+      quantity: params[:quantity],
+      color: shirt.color,
+      shirt_id: params[:id]
+    }
+    buyer = Buyer.create(buyer_hash)
+  else
+    shirt = Shirt.find_by({id: params[:id]})
+    binding.pry
+    buyer_hash = {
+      name: buyer[:name],
+      email: buyer[:email],
+      quantity: params[:quantity],
+      color: shirt.color,
+      shirt_id: params[:id]
+    }
+    buyer.update(buyer_hash)
+  end
   erb :continue, locals: { buyers: buyer }
 
 end
 
 get ('/checkout') do
-	erb :return
+
+
+  erb :return
 end
 
 get ('/admin') do
